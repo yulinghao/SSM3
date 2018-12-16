e2d296204df56e5a015d09e1ea99f58a4da173d7
yulinghao <1007862697@qq.com>
2018-12-16 23:36:04 +0800

    Initial commit

---
+++ src/main/java/com/qf/admin/service/impl/UserService.java
@@ -0,0 +1,9 @@
+package com.qf.admin.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class UserService {
+
+}
---
+++ src/main/java/com/qf/admin/web/UserAction.java
@@ -0,0 +1,16 @@
+//package com.qf.admin.web;
+//
+//import com.qf.admin.service.impl.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class UserAction {
+//@Autowired
+//private UserService userService;
+//@GetMapping("inde")
+//    public String index(){
+//        return "index";
+//    }
+//}
